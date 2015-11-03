9f86864c3fa7b46ac8d24742a34d7ebef420a312
mldn <mldnqa@163.com>
2015-11-03 13:35:01 +0800

    add $

--- Hello.java
+++ Hello.java
@@ -1,7 +1,7 @@
 ﻿public class Hello{
 	public static void main(String args[]){
 <<<<<<< HEAD
-		System.out.println("Hello GIT!@@@##@") ;
+		System.out.println("Hello GIT!@@@##@$") ;
 =======
 		System.out.println("Hello GIT!!!") ;
 >>>>>>> dev
